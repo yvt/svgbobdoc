08e37c74a62abce3c646ac735f29d8196c60b3c9
yvt <i@yvt.jp>
2019-05-30 13:09:22 +0900

    rustfmt

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,12 +1,10 @@
 // These features are required to apply custom attributes on modules.
 #![feature(proc_macro_hygiene)]
 #![feature(custom_inner_attributes)]
-
 // The use of `#[cfg_attr(rustdoc, ...)]` is recommended to improve
 // the compilation time on non-doc builds.
 // Unfortunately, it's currently feature-gated.
 #![feature(doc_cfg)]
-
 // TODO: Get this working... Hopefully when they are stabilized.
 // #![svgbobdoc::transform]
 
@@ -74,6 +72,3 @@ impl TestStruct {
     /// ```
     pub fn test_method() {}
 }
-
-
-
